72a10b2d10f06efac311729910a9eebf5c37b59c
Prince Owusu <powusu050@gmail.com>
2025-08-31 10:15:28 +0000

    updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,3 @@
-// import type { Metadata } from 'next';
-// import { Inter } from 'next/font/google';
-// import './globals.css';
-// import Navbar from '@/components/Navbar';
-// import { AuthProvider } from '@/context/AuthContext';
-// import Footer from '@/components/Footer';
-// import { Toaster } from '@/components/ui/toast';
-
-// const inter = Inter({ subsets: ['latin'] });
-
-// export const metadata: Metadata = {
-//   title: 'Cheap Rides Gh - Quality Used Cars in Ghana',
-//   description: 'Find quality used cars at unbeatable prices in Ghana. Your dream car is just a click away!',
-// };
-
-// export default function RootLayout({ children }: { children: React.ReactNode }) {
-//   return (
-//     <html lang="en">
-//       <body>
-//         <AuthProvider>
-//           {/* ✅ Now Navbar can safely use useAuth() */}
-//           <Navbar />
-          
-//           {children}
-//           {/* <Footer /> */}
-//           <Toaster />
-//         </AuthProvider>
-//       </body>
-//     </html>
-//   );
-// }
-
-// src/app/layout.tsx
 "use client";
 
 import "./globals.css";
@@ -40,6 +7,7 @@ import Loader from "@/components/Loader";
 import { AuthProvider } from "@/context/AuthContext";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "@/components/ui/toast";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   const [loading, setLoading] = useState(false);
@@ -55,6 +23,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body>
         <AuthProvider>
+          <Analytics/>
           <Navbar />
           {loading && <Loader />}
           {children}
