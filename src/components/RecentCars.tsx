d277f6721140f7af8002beaf24a8683544a9892d
Prince Owusu <powusu050@gmail.com>
2025-09-06 18:42:19 +0000

    recent cars

--- src/components/RecentCars.tsx
+++ src/components/RecentCars.tsx
@@ -62,7 +62,7 @@ export default function RecentCars() {
         
 
         {recentCars.length > 0 ? (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
 
           {recentCars.map((car) => (
               <CarCard key={car.id} car={car} />
